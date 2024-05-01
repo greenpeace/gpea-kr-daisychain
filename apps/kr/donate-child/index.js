7cc84d5c3c2adf3fe53bcbfbff4b3af2982e2161
jackson <vvvvv0214@gmail.com>
2024-05-01 17:13:14 +0900

    Refactor query object keys to lowercase and add useRouter hook in donate-child/index.js

--- apps/kr/donate-child/index.js
+++ apps/kr/donate-child/index.js
@@ -7,6 +7,7 @@ import SEO from './SEO';
 
 function Index() {
     const theme = useSelector((state) => state?.theme);
+    const { query } = useRouter();
     const lowerCaseQuery = Object.keys(query).reduce((result, key) => {
         result[ key.toLowerCase() ] = query[ key ];
         return result;
